199e41be1d5d9c9e1a2230b7272f3913ecbf24e6
QuocHuynh <quochuynh0409@gmail.com>
2022-09-20 16:43:27 +0700

    Revert "Update file URLs"
    This reverts commit 37d5952a651cd8d39849a4c6a3973299caa4014e.

--- EasyGoTravelApp/app/src/main/java/com/example/easygo_travelapp/utilities/URLs.java
+++
@@ -1,8 +0,0 @@
-package com.example.easygo_travelapp.utilities;
-
-final public class URLs {
-
-    static public final String destinationWeLove = "destinationWeLove";
-    static public final String scenics = "scenics";
-
-}
